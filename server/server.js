63562824f71083fadc73c1add30497cfb00fc163
DineshLogan <dineshlogan31@gmail.com>
2023-06-30 14:23:21 +0530

    save

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ require("dotenv").config()
 
 const app=express()
 app.use(express.json())
+app.use(function(req,res,next)
+{
+    res.setHeader({
+        "Access-Control-Allow-Origin" : "http://localhost:3000",
+        "Access-Control-Allow-Credentials" : true,
+        "Access-Control-Allow-Methods" : GET, POST, OPTIONS,
+        "Access-Control-Allow-Headers" : "Origin, Content-Type, Accept"
+    })
+    next()
+})
 const corsOptions ={
     origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
