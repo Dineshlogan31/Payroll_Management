443e9af2e0b832863f944f6760ca20f53a081268
DineshLogan <dineshlogan31@gmail.com>
2023-07-02 20:12:56 +0530

    save

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const app=express()
 app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://payroll-management-3od1.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
   });
 
