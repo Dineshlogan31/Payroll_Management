b38968dc3fae478f69851bf400e17574224cf54b
DineshLogan <dineshlogan31@gmail.com>
2023-07-02 20:08:49 +0530

    save

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ require("dotenv").config()
 
 const app=express()
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://payroll-management-3od1.vercel.app');
+    next();
+  });
 
 
 app.use(cors())
