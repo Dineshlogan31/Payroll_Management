20679f23a0af910ff3b50d5ca258442a2f8aea4a
DineshLogan <dineshlogan31@gmail.com>
2023-07-05 15:54:16 +0530

    save

--- client/src/Pages/Mailcomponent.js
+++ client/src/Pages/Mailcomponent.js
@@ -12,6 +12,14 @@ const Mailcomponent = () => {
   useEffect(()=>{
     dispatch(getAllLeaveAppliedByEmployee())
   },[dispatch])
+
+  const calculateDays = (startDate, endDate) => {
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const diffInTime = end.getTime() - start.getTime();
+    const diffInDays = diffInTime / (1000 * 3600 * 24);
+    return diffInDays;
+  }
   return (
     <div className='mail-container'>
     <h1 style={{textAlign:"center"}}>MailBox</h1><br></br>
@@ -19,9 +27,14 @@ const Mailcomponent = () => {
       <tbody>
     {
       leaveAlliedByEmployee.map((leaves,index)=>{
+     
+        const reversedStartDate = new Date(leaves.startDate).toLocaleDateString();
+        const reversedEndDate = new Date(leaves.endDate).toLocaleDateString();
+        const daysBetween = calculateDays(leaves.startDate, leaves.endDate);
         return (
+          
           <tr key={index}>
-            <td >{leaves.employeeName} applied {leaves.leaveType} for 2 days from {leaves.startDate.reverse} to {leaves.endDate} <Button variant='success'>Approved</Button> <Button>Reject</Button></td>
+            <td >{leaves.employeeName} applied {leaves.leaveType} for {daysBetween} days from {reversedStartDate} to {reversedEndDate} <Button variant='success'>Approved</Button> <Button>Reject</Button></td>
           </tr>
         )
       })
